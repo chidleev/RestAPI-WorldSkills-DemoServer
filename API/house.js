c8c6227bb9254b01b6f3b4e46ac7a43e07b585c1
chidleev <chidleev22@gmail.com>
2022-06-25 13:07:02 +0300

    25,06,22 13,06

--- API/house.js
+++ API/house.js
@@ -12,14 +12,6 @@ API.post('/', (req, res) => {
         errors: {}
     }
 
-    
-    
-    
-    
-    
-    
-    built_quarter
-
     if (!req.body.project_id) {
         error.errors.project_id = ["Поле ID объекта не может быть пустым"]
     }
@@ -33,10 +25,17 @@ API.post('/', (req, res) => {
     }
 
     if (!req.body.built_year) {
-        error.errors.built_year = ["Поле сайта объекта не может быть пустым"]
+        error.errors.built_year = ["Поле года сдачи не может быть пустым"]
     }
-    else if (!validator.isURL(req.body.website)) {
-        error.errors.website = ["Поле сайта объекта должно быть ссылкой"]
+    else if ((req.body.built_year < 1000) || (req.body.built_year > 9999)) {
+        error.errors.built_year = ["Поле года сдачи должно быть четырехзначным числом"]
+    }
+
+    if (!req.body.built_quarter) {
+        error.errors.built_quarter = ["Поле квартала сдачи не может быть пустым"]
+    }
+    else if ((req.body.built_quarter < 1) || (req.body.built_quarter > 4)) {
+        error.errors.built_quarter = ["Поле квартала сдачи должно быть больше 1 и меньше 4"]
     }
 
     if (Object.keys(error.errors).length) {
@@ -44,17 +43,116 @@ API.post('/', (req, res) => {
         return
     }
 
-    dataBase.Projects.create(req.body)
-        .then(project => {
-            res.json({
-                data: {
-                    id: project.id
-                }
+    dataBase.Projects.findByPk(req.body.project_id)
+    .then(project => {
+        if (project) {
+            dataBase.Houses.create(req.body)
+            .then(house => {
+                project.addHouse(house)
+                .then(project => {
+                    res.json({
+                        data: {
+                            id: house.id
+                        }
+                    })
+                })
+                .catch(error => {
+                    error.code = 500
+                    error.message = "Не удалось записать данные о новом доме"
+                    res.status(error.code).json({ error })
+                })
             })
+            .catch(error => {
+                error.code = 500
+                error.message = "Не удалось записать данные о новом доме"
+                res.status(error.code).json({ error })
+            })
+        }
+        else {
+            error.code = 404
+            error.errors.project_id = ["Нет проекта с таким ID"]
+            res.status(error.code).json({ error })
+        }
+    })
+    .catch(error => {
+        error.code = 500
+        error.message = 'Sequelize Error'
+        res.status(error.code).json({ error })
+    })
+})
+
+API.get('/:houseId', (req, res) => {
+    dataBase.Houses.findByPk(req.params.houseId, {
+        attributes: {
+            exclude: ['ProjectId', 'projectIdId']
+        },
+        include: [{
+            model: dataBase.Sections,
+            as: 'sections',
+            attributes: ['id', 'number']
+        }]
+    })
+        .then(house => {
+            if (house) {
+                res.json({
+                    data: house
+                })
+            }
+            else res.sendStatus(404)
         })
         .catch(error => {
             error.code = 500
-            error.message = "Не удалось записать данные о новом проекте"
+            error.message = "Не удалось получить данные о доме"
+            res.status(error.code).json({ error })
+        })
+})
+
+API.patch('/:houseId', (req, res) => {
+    const error = {
+        code: 422,
+        message: "Validation error",
+        errors: {}
+    }
+    
+    if (!req.body.name) {
+        error.errors.name = ["Поле названия дома не может быть пустым"]
+    }
+
+    if (!req.body.address) {
+        error.errors.address = ["Поле адреса объекта не может быть пустым"]
+    }
+
+    if (!req.body.built_year) {
+        error.errors.built_year = ["Поле года сдачи не может быть пустым"]
+    }
+    else if ((req.body.built_year < 1000) || (req.body.built_year > 9999)) {
+        error.errors.built_year = ["Поле года сдачи должно быть четырехзначным числом"]
+    }
+
+    if (!req.body.built_quarter) {
+        error.errors.built_quarter = ["Поле квартала сдачи не может быть пустым"]
+    }
+    else if ((req.body.built_quarter < 1) || (req.body.built_quarter > 4)) {
+        error.errors.built_quarter = ["Поле квартала сдачи должно быть больше 1 и меньше 4"]
+    }
+
+    if (Object.keys(error.errors).length) {
+        res.status(error.code).json({ error })
+        return
+    }
+
+    dataBase.Houses.update(req.body, {
+        where: {
+            id: +req.params.houseId //плюс впереди стоит для того, чтобы сделать из строки число
+        }
+    })
+        .then(project => {
+            if (project) res.sendStatus(204)
+            else res.sendStatus(404)
+        })
+        .catch(error => {
+            error.code = 500
+            error.message = "Не удалось записать данные об изменении проекта"
             res.status(error.code).json({ error })
         })
 })
--- API/section.js
+++ API/section.js
@@ -6,7 +6,38 @@ const dataBase = require('../dataBase')
 const API = express()
 
 API.post('/', (req, res) => {
+    const error = {
+        code: 422,
+        message: "Validation error",
+        errors: {}
+    }
 
+    if (!req.body.house_id) {
+        error.errors.house_id = ["Поле ID дома не может быть пустым"]
+    }
+
+    if (!req.body.number) {
+        error.errors.number = ["Поле номера подъезда не может быть пустым"]
+    }
+
+    if (!req.body.floors) {
+        error.errors.floors = ["Поле количества этажей подъезда не может быть пустым"]
+    }
+
+    if (!req.body.flats_on_floor) {
+        error.errors.flats_on_floor = ["Поле количества квартир на этаже подъезда не может быть пустым"]
+    }
+
+    if (!req.body.starting_flat_number) {
+        error.errors.starting_flat_number = ["Поле номера с которого начинается нумерация квартир не может быть пустым"]
+    }
+
+    if (Object.keys(error.errors).length) {
+        res.status(error.code).json({ error })
+        return
+    }
+
+    
 })
 
 module.exports = API
